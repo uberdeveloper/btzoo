cbd07e37838ea4ae3e0900175c62ff0ea867df74
machine <uberdeveloper001@gmail.com>
2019-12-17 21:34:27 +0530

    initial metrics added

--- performance.py
+++ performance.py
@@ -2,7 +2,7 @@
 This module calculates different performance metrics for the 
 backtests and saves them in a summary folder
 """
-
+import os
 import pandas as pd
 import numpy as np
 import pyfolio as pf
@@ -21,10 +21,25 @@ def get_benchmark(filename='indices.csv', parse_dates=['Date']):
 	df.rename(lambda x: x.lower(), axis='columns', inplace=True)
 	df = df.set_index('date').sort_index()
 	df['chg'] = df.close.pct_change(1)
+	df = df.loc['2012':]
 	return df
 
 def main():
 	benchmark = get_benchmark()
+	DIR = '/media/machine/4E1EA2D152455460/temp/btzoo_results/results'
+	counter = 0
+	for root,directory,files in os.walk(DIR):
+		for file in files:
+			if file.endswith('h5'):
+				filename = os.path.join(root, file)
+				dct = {}
+				results = pd.read_hdf(filename)
+				byday = results.groupby('timestamp').net_profit.sum()
+				stats = pf.timeseries.perf_stats(byday/100000,
+					factor_returns=benchmark.chg)
+				dct.update(stats)
+				simple = metrics(results)
+				dct.update(simple)
 
 if __name__ == "__main__":
 	main()
