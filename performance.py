5949dc7de8b619de5cf23830c6dc399ff9702639
machine <uberdeveloper001@gmail.com>
2019-12-17 21:44:09 +0530

    metrics moved to a separate function

--- performance.py
+++ performance.py
@@ -24,6 +24,26 @@ def get_benchmark(filename='indices.csv', parse_dates=['Date']):
 	df = df.loc['2012':]
 	return df
 
+def all_metrics(result, benchmark):
+	"""
+	Calculate all the metrics for the given backtest results
+	result
+		backtest results in the expected format - results generated
+		by rapid
+	"""
+	dct = {}
+	byday = result.groupby('timestamp').net_profit.sum()
+	stats = pf.timeseries.perf_stats(byday/100000,
+		factor_returns=benchmark.chg)
+	dct.update(stats.to_dict())
+	simple = metrics(result)
+	dct.update(simple)
+	dct['open==high'] = result.query('open==high').net_profit.sum()
+	dct['open==low'] = result.query('open==low').net_profit.sum()
+	by_year = byday.groupby(lambda x: x.year).sum()
+	dct.update(by_year.to_dict())
+	return dct
+
 def main():
 	benchmark = get_benchmark()
 	DIR = '/media/machine/4E1EA2D152455460/temp/btzoo_results/results'
@@ -32,18 +52,8 @@ def main():
 		for file in files:
 			if file.endswith('h5'):
 				filename = os.path.join(root, file)
-				dct = {}
 				results = pd.read_hdf(filename)
-				byday = results.groupby('timestamp').net_profit.sum()
-				stats = pf.timeseries.perf_stats(byday/100000,
-					factor_returns=benchmark.chg)
-				dct.update(stats.to_dict())
-				simple = metrics(results)
-				dct.update(simple)
-				dct['open==high'] = results.query('open==high').net_profit.sum()
-				dct['open==low'] = results.query('open==low').net_profit.sum()
-				by_year = byday.groupby(lambda x: x.year).sum()
-				dct.update(by_year.to_dict())
+				perf_stats = all_metrics(results, benchmark)
 
 if __name__ == "__main__":
 	main()
