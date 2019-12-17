f812827147b896ce1ff2138d5ceb0b956dceb43e
machine <uberdeveloper001@gmail.com>
2019-12-17 20:16:37 +0530

    performance module added

---
+++ performance.py
@@ -0,0 +1,15 @@
+"""
+This module calculates different performance metrics for the 
+backtests and saves them in a summary folder
+"""
+
+import pandas as pd
+import numpy as np
+import pyfolio as pf
+from fastbt.rapid import metrics
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
