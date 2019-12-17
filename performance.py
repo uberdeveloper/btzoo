8d640f9cd64e530ff7c472eae9fe58a53d4efc54
machine <uberdeveloper001@gmail.com>
2019-12-17 20:58:36 +0530

    benchmark added

--- performance.py
+++ performance.py
@@ -8,8 +8,23 @@ import numpy as np
 import pyfolio as pf
 from fastbt.rapid import metrics
 
+def get_benchmark(filename='indices.csv', parse_dates=['Date']):
+	"""
+	Get the benchmark returns
+	filename
+		filename with date and ohlc columns
+	parse_dates
+		columns to parse dates as list
+	returns calculated on the close column
+	"""
+	df = pd.read_csv(filename, parse_dates=parse_dates)
+	df.rename(lambda x: x.lower(), axis='columns', inplace=True)
+	df = df.set_index('date').sort_index()
+	df['chg'] = df.close.pct_change(1)
+	return df
+
 def main():
-	pass
+	benchmark = get_benchmark()
 
 if __name__ == "__main__":
 	main()
